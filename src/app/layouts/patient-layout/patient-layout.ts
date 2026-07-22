24cd0348467e44dc7e6bc8ba7de6abaa08964647
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-07-22 23:37:23 +0300

    add layouts components

---
+++ src/app/layouts/patient-layout/patient-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-layout',
+  imports: [],
+  templateUrl: './patient-layout.html',
+  styleUrl: './patient-layout.css',
+})
+export class PatientLayout {
+
+}
